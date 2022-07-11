1ce9e23a18f681dd6b9b42f753ec2d509d142f15
Chava ALV <msalvarado.lopez@gmail.com>
2022-07-10 22:47:17 -0500

    fullscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -6,13 +6,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild("documentElement") docElement: ElementRef
+
+  globalInstance: any;    
   title = 'dcTimeApp';
 
-  constructor() { }
+  constructor(private _render: Renderer2, private _elem: ElementRef) { }
 
   ngOnInit() {
-    
+    setTimeout(() => {
+      this._elem.nativeElement.click();
+    }, 5000);
+  }
+
+  ngAfterViewInit(): void {
+    this.globalInstance = this._render.listen(this._elem.nativeElement, 'click', () => {
+      if(this._elem.nativeElement.requestFullScreen) {
+        this._elem.nativeElement.requestFullScreen();
+      } else if(this._elem.nativeElement.mozRequestFullScreen) {
+        this._elem.nativeElement.mozRequestFullScreen();
+      } else if(this._elem.nativeElement.webkitRequestFullScreen) {
+        this._elem.nativeElement.webkitRequestFullScreen();
+      }
+  });
   }
 
 }
